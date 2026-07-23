73795f7f7003cefb5a8ac99bf1cf466ea7051e1f
vanessaputr1 <sarasputri87@gmail.com>
2026-07-23 21:03:29 +0700

    uas

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,44 +8,45 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $totalUsers = 0;
-        $totalEvents = 0;
-        $activeEvents = 0;
-        $pendingOrders = 0;
-        $ticketsSold = 0;
-        $totalRevenue = 0;
+        // $totalUsers = 0;
+        // $totalEvents = 0;
+        // $activeEvents = 0;
+        // $pendingOrders = 0;
+        // $ticketsSold = 0;
+        // $totalRevenue = 0;
 
-        $recentTransactions = collect();
+        // $recentTransactions = collect();
 
-        $months = [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'Mei',
-            'Jun',
-            'Jul',
-            'Agu',
-            'Sep',
-            'Okt',
-            'Nov',
-            'Des'
-        ];
+        // $months = [
+        //     'Jan',
+        //     'Feb',
+        //     'Mar',
+        //     'Apr',
+        //     'Mei',
+        //     'Jun',
+        //     'Jul',
+        //     'Agu',
+        //     'Sep',
+        //     'Okt',
+        //     'Nov',
+        //     'Des'
+        // ];
 
-        $userData = array_fill(0, 12, 0);
-        $eventData = array_fill(0, 12, 0);
+        // $userData = array_fill(0, 12, 0);
+        // $eventData = array_fill(0, 12, 0);
 
-        return view('admin.dashboard', compact(
-            'totalUsers',
-            'totalEvents',
-            'activeEvents',
-            'pendingOrders',
-            'ticketsSold',
-            'totalRevenue',
-            'recentTransactions',
-            'months',
-            'userData',
-            'eventData'
-        ));
+        // return view('admin.dashboard', compact(
+        //     'totalUsers',
+        //     'totalEvents',
+        //     'activeEvents',
+        //     'pendingOrders',
+        //     'ticketsSold',
+        //     'totalRevenue',
+        //     'recentTransactions',
+        //     'months',
+        //     'userData',
+        //     'eventData'
+        // ));
+        return "Dashboard Berhasil";
     }
 }
